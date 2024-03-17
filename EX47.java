e85b4454c767d55983cc2d4d06211b41471ed2bb
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-03-17 17:04:41 -0300

    update

---
+++ EX47.java
@@ -0,0 +1,16 @@
+package Java_Exercicios;
+
+public class EX47 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int matriz[][] = new int[4][3];
+		for(int i = 0;i<4;i++) {
+			System.out.printf("%da. linha: ",(i+1));
+			for(int j =0;i<3;i++) {
+				System.out.println(matriz[i][j]);
+			}
+		}
+	}
+
+}
