ad062dbc988541c44b4ad289aeee5f94b2d5d9a6
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-03-17 14:26:17 -0300

    Update EX35.java

--- EX35.java
+++ EX35.java
@@ -4,6 +4,32 @@ import java.util.Calendar;
 
 public class EX35 {
 
+//3.5 Construir um programa que obtenha a data do sistema e mostra essa informação com o nome do mês por extenso, usando switch.  
+
+//Utilizar o exemplo abaixo, que utiliza a classe Calendar, como base e faz a captura do dia do mês:
+/*
+	Calendar calendario = Calendar.getInstance();
+	
+	int dia = calendario.get(Calendar.DAY_OF_MONTH);
+	System.out.printf("Dia = %d", dia);
+	
+	
+	Notem que a classe Calendar de java.util deve ser importada.
+	Para a leitura dos valores do mês e do ano, usa Calendar.<método>, como foi construído para o dia.
+	Ainda, a contagem dos meses começa em 0-janeiro, 1-fevereiro, ...
+	
+	OBSERVAÇÃO
+
+	Se for só para mostrar a data e hora do sistema pode usar
+	// importar java.util.Date
+	System.out.println("Data = "+DateFormat.getDateInstance().format(new Date()));
+	System.out.println("Hora = "+DateFormat.getTimeInstance().format(new Date()));
+	Vamos trabalhar especificamente com datas em uma aula utilizando a classe Calendar e outras classes específicas para datas.
+
+
+	
+*/
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calendar calendario = Calendar.getInstance();
